cd95e888e52c77e1e269c8c6b95bcb469d9719cf
Brian Bingham <briansbingham@gmail.com>
2019-05-10 17:30:20 -0700

    adding monolithic launch file for two robots

--- scripts/remote_tb3_launch.sh
+++ scripts/remote_tb3_launch.sh
@@ -1,3 +1,9 @@
 ROS_MASTER_URI=http://192.168.11.110:11311 ROS_HOSTNAME=192.168.11.118 ROS_NAMESPACE=tb3_1 roslaunch turtlebot3_bringup turtlebot3_robot.launch multi_robot_name:="tb3_1" set_lidar_frame_id:="tb3_1/base_scan"
 
 ROS_MASTER_URI=http://192.168.11.110:11311 ROS_HOSTNAME=192.168.11.113 ROS_NAMESPACE=tb3_0 roslaunch turtlebot3_bringup turtlebot3_robot.launch multi_robot_name:="tb3_0" set_lidar_frame_id:="tb3_0/base_scan"
+
+
+ROS_MASTER_URI=http://192.168.11.110:11311 ROS_HOSTNAME=192.168.11.118 roslaunch turtlebot3_bringup turtlebot3_robot.launch
+
+
+export ROS_MASTER_URI=http://192.168.11.110:11311 && roslaunch thingmagic_rfid reader.launch port:=/dev/ttyACM1
