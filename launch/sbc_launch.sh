0a9770e0898ef5008148c27000c78149e8d5dd66
Brian Bingham <briansbingham@gmail.com>
2019-06-03 14:37:15 -0700

    two move base instances

---
+++ launch/sbc_launch.sh
@@ -0,0 +1,5 @@
+
+ROS_MASTER_URI=http://192.168.11.110:11311 ROS_NAMESPACE=tb3_0 roslaunch turtlebot3_bringup turtlebot3_robot.launch multi_robot_name:="tb3_0" set_lidar_frame_id:="tb3_0/base_scan"
+
+
+ROS_MASTER_URI=http://192.168.11.110:11311 ROS_NAMESPACE=tb3_1 roslaunch turtlebot3_bringup turtlebot3_robot.launch multi_robot_name:="tb3_1" set_lidar_frame_id:="tb3_1/base_scan"
--- scripts/pub_cmdvel_stop.sh
+++ scripts/pub_cmdvel_stop.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rostopic pub -1 /cmd_vel geometry_msgs/Twist -- '[0.0, 0.0, 0.0]' '[0.0, 0.0, 0.0]'
+rostopic pub -1 cmd_vel geometry_msgs/Twist -- '[0.0, 0.0, 0.0]' '[0.0, 0.0, 0.0]'
